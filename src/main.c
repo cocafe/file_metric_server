52762fd2717cd17ee7cbe5e86b3563187e2d430d
Ji Huang <cocafehj@gmail.com>
2025-03-02 19:34:10 +0800

    support to metric a directory
    Signed-off-by: Ji Huang <cocafehj@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +7,12 @@
 #include <signal.h>
 #include <unistd.h>
 #include <math.h>
+#include <ftw.h>
+#include <libgen.h>
+
+#include <sys/stat.h>
+
+#include <uthash.h>
 
 #include <libjj/list.h>
 #include <libjj/logging.h>
@@ -30,16 +37,19 @@ lopt_uint_simple(listen_port, "Listen port for metric server");
 static jbuf_t jkey_root;
 
 #define METRIC_LABEL_MAX                (5)
+#define METRIC_LABEL_STRING_SIZE        (64)
 
 struct metric_label {
         struct list_head node;
-        prom_label l;
+        char key[METRIC_LABEL_STRING_SIZE];
+        char value[METRIC_LABEL_STRING_SIZE];
 };
 
 struct metric {
         struct list_head node;
         struct list_head labels;
         prom_metric_def def;
+        uint32_t fresh_sec;     // last modified in secs
         char path[PATH_MAX];
         char *expr;
         double val;
@@ -55,8 +65,52 @@ struct metric_set {
 
 static LIST_HEAD(metric_list);
 static LIST_HEAD(metric_set_list);
+static LIST_HEAD(metric_dir_list);
 static prom_metric_set metric_set;
 static prom_server prom_srv;
+
+struct label_cache {
+        char *buf;
+        UT_hash_handle hh;
+};
+
+static struct label_cache *label_caches = NULL;
+
+char *label_cache_get(char *value)
+{
+        struct label_cache *c = NULL;
+
+        HASH_FIND_STR(label_caches, value, c);
+
+        if (c) {
+                return c->buf;
+        } else {
+                size_t len = strlen(value);
+                char *s = calloc(len + 2, sizeof(char));
+                c = calloc(1, sizeof(struct label_cache));
+                if (!s || !c)
+                        return NULL;
+
+                strncpy(s, value, len);
+
+                c->buf = s;
+                HASH_ADD_STR(label_caches, buf, c);
+
+                return s;
+        }
+}
+
+void label_cache_free(void)
+{
+        struct label_cache *s, *t;
+
+        HASH_ITER(hh, label_caches, s, t) {
+                if (s->buf)
+                        free(s->buf);
+
+                free(s);
+        }
+}
 
 int is_gonna_exit(void)
 {
@@ -128,6 +182,7 @@ static int config_root_key_create(jbuf_t *b)
                                         void *obj_metric = jbuf_offset_obj_open(b, NULL, 0);
 
                                         jbuf_offset_add(b, strptr, "expr", offsetof(struct metric, expr));
+                                        jbuf_offset_add(b, u32, "fresh_sec", offsetof(struct metric, fresh_sec));
                                         jbuf_offset_strbuf_add(b, "path", offsetof(struct metric, path), sizeof(((struct metric *)(0))->path));
 
                                         {
@@ -143,8 +198,8 @@ static int config_root_key_create(jbuf_t *b)
                                                 {
                                                         void *obj_label = jbuf_offset_obj_open(b, NULL, 0);
 
-                                                        jbuf_offset_add(b, strptr, "key", offsetof(struct metric_label, l.key));
-                                                        jbuf_offset_add(b, strptr, "val", offsetof(struct metric_label, l.value));
+                                                        jbuf_offset_strbuf_add(b, "key", offsetof(struct metric_label, key), sizeof(sizeof(((struct metric_label *)(0))->key)));
+                                                        jbuf_offset_strbuf_add(b, "val", offsetof(struct metric_label, value), sizeof(sizeof(((struct metric_label *)(0))->value)));
 
                                                         jbuf_obj_close(b, obj_label);
                                                 }
@@ -162,6 +217,73 @@ static int config_root_key_create(jbuf_t *b)
                 }
 
                 jbuf_arr_close(b, arr_metric_sets);
+        }
+
+        {
+                void *arr_metric_dirs = jbuf_list_arr_open(b, "metric_dirs");
+                jbuf_list_arr_setup(b,
+                                    arr_metric_dirs,
+                                    &metric_dir_list,
+                                    sizeof(struct metric_set),
+                                    offsetof(struct metric_set, node),
+                                    0, 0);
+
+                {
+                        void *obj_metric_set = jbuf_offset_obj_open(b, NULL, 0);
+
+                        jbuf_offset_add(b, strptr, "name", offsetof(struct metric_set, def.name));
+                        jbuf_offset_add(b, strptr, "help", offsetof(struct metric_set, def.help));
+                        jbuf_offset_add(b, strptr, "type", offsetof(struct metric_set, def.type));
+
+                        {
+                                void *arr_metrics = jbuf_list_arr_open(b, "metrics");
+
+                                jbuf_offset_list_arr_setup(b,
+                                                           arr_metrics,
+                                                           offsetof(struct metric_set, metrics),
+                                                           sizeof(struct metric),
+                                                           offsetof(struct metric, node),
+                                                           0, 0);
+
+                                {
+                                        void *obj_metric = jbuf_offset_obj_open(b, NULL, 0);
+
+                                        jbuf_offset_add(b, strptr, "expr", offsetof(struct metric, expr));
+                                        jbuf_offset_add(b, u32, "fresh_sec", offsetof(struct metric, fresh_sec));
+                                        jbuf_offset_strbuf_add(b, "path", offsetof(struct metric, path), sizeof(((struct metric *)(0))->path));
+
+                                        {
+                                                void *arr_labels = jbuf_list_arr_open(b, "labels");
+
+                                                jbuf_offset_list_arr_setup(b,
+                                                                           arr_labels,
+                                                                           offsetof(struct metric, labels),
+                                                                           sizeof(struct metric_label),
+                                                                           offsetof(struct metric_label, node),
+                                                                           0, 0);
+
+                                                {
+                                                        void *obj_label = jbuf_offset_obj_open(b, NULL, 0);
+
+                                                        jbuf_offset_strbuf_add(b, "key", offsetof(struct metric_label, key), sizeof(sizeof(((struct metric_label *)(0))->key)));
+                                                        jbuf_offset_strbuf_add(b, "val", offsetof(struct metric_label, value), sizeof(sizeof(((struct metric_label *)(0))->value)));
+
+                                                        jbuf_obj_close(b, obj_label);
+                                                }
+
+                                                jbuf_arr_close(b, arr_labels);
+                                        }
+
+                                        jbuf_obj_close(b, obj_metric);
+                                }
+
+                                jbuf_arr_close(b, arr_metrics);
+                        }
+
+                        jbuf_obj_close(b, obj_metric_set);
+                }
+
+                jbuf_arr_close(b, arr_metric_dirs);
         }
 
         jbuf_obj_close(b, root);
@@ -232,7 +354,7 @@ static int metric_set_element_validate(struct metric *m, char *name)
                 }
 
                 list_for_each_entry_safe(p, n, &m->labels, node) {
-                        if (is_strptr_not_set(p->l.key) || is_strptr_not_set(p->l.value)) {
+                        if (is_strptr_not_set(p->key) || is_strptr_not_set(p->value)) {
                                 pr_err("metric \"%s\" delete invalid empty label for path \"%s\"\n", name, m->path);
                                 list_del(&p->node);
                         }
@@ -247,11 +369,11 @@ static int metric_set_element_validate(struct metric *m, char *name)
         return 0;
 }
 
-static int metric_set_list_validate(void)
+static int metric_set_list_validate(struct list_head *head)
 {
         struct metric_set *p;
 
-        list_for_each_entry(p, &metric_set_list, node) {
+        list_for_each_entry(p, head, node) {
                 struct metric *m;
 
                 if (metric_def_validate(&p->def)) {
@@ -273,7 +395,8 @@ static int metric_set_list_validate(void)
 static int config_validate(void)
 {
         metric_list_validate();
-        metric_set_list_validate();
+        metric_set_list_validate(&metric_set_list);
+        metric_set_list_validate(&metric_dir_list);
 
         return 0;
 }
@@ -290,9 +413,27 @@ static int metric_value_get(struct metric *m)
                 return -errno;
         }
 
+        if (m->fresh_sec) {
+                struct stat file_stat;
+                time_t now = time(NULL);
+
+                if (stat(m->path, &file_stat) == -1) {
+                        pr_err("failed to stat \"%s\", %s\n", m->path, strerror(abs(errno)));
+                        err = -errno;
+                        goto out;
+                }
+
+                if (now - file_stat.st_mtime > m->fresh_sec) {
+                        pr_verbose("file \"%s\" is out-dated\n", m->path);
+                        err = -EAGAIN;
+                        goto out;
+                }
+        }
+
         if (fgets(buf, sizeof(buf), file) == NULL) {
                 pr_err("failed to read \"%s\", %s\n", m->path, strerror(abs(errno)));
-                return -errno;
+                err = -errno;
+                goto out;
         }
 
         m->val = strtod(buf, NULL);
@@ -301,6 +442,7 @@ static int metric_value_get(struct metric *m)
                 err = -ERANGE;
         }
 
+out:
         fclose(file);
 
         return err;
@@ -376,7 +518,8 @@ static int metric_set_list_refresh(void)
                                 struct metric_label *p;
 
                                 list_for_each_entry(p, &m->labels, node) {
-                                        memcpy(&labels[n], &p->l, sizeof(p->l));
+                                        labels[n].key = p->key;
+                                        labels[n].value = p->value;
                                         n++;
                                 }
 
@@ -390,10 +533,118 @@ static int metric_set_list_refresh(void)
         return 0;
 }
 
+static struct metric *curr_dir_metric;
+static struct metric_set *curr_dir_metric_set;
+
+int dir_file_cb(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+        struct metric *m = curr_dir_metric;
+        struct metric_set *set = curr_dir_metric_set;
+        char *filename = basename((char *)path);
+        char buf[256] = { };
+        FILE *file = NULL;
+
+        if (typeflag != FTW_F)
+                return 0;
+
+        pr_verbose("open %s\n", path);
+
+        file = fopen(path, "r");
+        if (file == NULL) {
+                pr_err("failed to open \"%s\", \"%s\"\n", path, strerror(errno));
+                goto out;
+        }
+
+        if (m->fresh_sec) {
+                struct stat file_stat;
+                time_t now = time(NULL);
+
+                if (stat(path, &file_stat) == -1) {
+                        pr_err("failed to stat \"%s\", %s\n", path, strerror(abs(errno)));
+                        goto out;
+                }
+
+                if (now - file_stat.st_mtime > m->fresh_sec) {
+                        pr_verbose("file \"%s\" is out-dated\n", path);
+                        goto out;
+                }
+        }
+
+        if (fgets(buf, sizeof(buf), file) == NULL) {
+                pr_err("failed to read \"%s\", %s\n", m->path, strerror(abs(errno)));
+                goto out;
+        }
+
+        m->val = strtod(buf, NULL);
+        if (errno == ERANGE) {
+                pr_err("failed to parse \"%s\" into data\n", m->path);
+                m->val = NAN;
+        }
+
+        if (!isnan(m->val) && m->expr) {
+                metric_value_expr_compute(m);
+        }
+
+        {
+                prom_label labels[PROM_MAX_LABELS] = {};
+                prom_metric *_m;
+                int n = 0;
+                struct metric_label *p;
+
+                list_for_each_entry(p, &m->labels, node) {
+                        labels[n].key = p->key;
+                        labels[n].value = label_cache_get(filename);
+
+                        n++;
+                }
+
+                _m = prom_label_metric_create_or_get(&metric_set, &set->def, n, labels);
+                if (_m)
+                        _m->value = m->val;
+        }
+
+out:
+        if (file)
+                fclose(file);
+
+        return 0;
+}
+
+static int metric_dir_list_refresh(void)
+{
+        struct metric_set *s;
+
+        list_for_each_entry(s, &metric_dir_list, node) {
+                struct metric *m;
+
+                curr_dir_metric_set = s;
+
+                if (s->disabled)
+                        continue;
+
+                list_for_each_entry(m, &s->metrics, node) {
+                        if (m->disabled)
+                                continue;
+
+                        curr_dir_metric = m;
+                        if (nftw(m->path, dir_file_cb, 20, FTW_PHYS) == -1) {
+                                pr_err("nftw(): %s\n", strerror(errno));
+                                continue;
+                        }
+                }
+        }
+
+        curr_dir_metric_set = NULL;
+        curr_dir_metric = NULL;
+
+        return 0;
+}
+
 static int prom_on_http_get(prom_server *srv, void *arg)
 {
         metric_list_refresh();
         metric_set_list_refresh();
+        metric_dir_list_refresh();
 
         prom_commit_start(srv);
         prom_commit(srv, &metric_set);
@@ -411,11 +662,11 @@ static void metric_list_register(void)
         }
 }
 
-static void metric_set_list_register(void)
+static void metric_set_list_register(struct list_head *head)
 {
         struct metric_set *p;
 
-        list_for_each_entry(p, &metric_set_list, node) {
+        list_for_each_entry(p, head, node) {
                 prom_metric_register(&metric_set, &p->def);
         }
 }
@@ -438,7 +689,8 @@ static int prom_srv_init(void)
         prom_server_on_http_get_cb(&prom_srv, prom_on_http_get, NULL);
 
         metric_list_register();
-        metric_set_list_register();
+        metric_set_list_register(&metric_set_list);
+        metric_set_list_register(&metric_dir_list);
 
         return err;
 }
@@ -513,7 +765,9 @@ int main(int argc, char *argv[])
         pr_info("loaded config:\n");
         jbuf_traverse_print(&jkey_root);
 
-        if (list_empty(&metric_list) && list_empty(&metric_set_list)) {
+        if (list_empty(&metric_list) &&
+            list_empty(&metric_set_list) &&
+            list_empty(&metric_dir_list)) {
                 pr_err("no metrics have been loaded\n");
                 return -ENODATA;
         }
@@ -526,6 +780,8 @@ int main(int argc, char *argv[])
         prom_srv_deinit();
 
         jbuf_deinit(&jkey_root);
+
+        label_cache_free();
 
         return err;
 }
